0854a2c39f08a664664e7ebf2b35edf157897546
Joshua <Joshua32149@gmail.com>
2023-10-30 15:49:46 -0400

    added new methods

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,6 +1,13 @@
 package com.pluralsight;
 
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.time.*;
+
 public class Employee {
+    static Scanner myScanner = new Scanner(System.in);
+    static LocalDateTime timeNow = LocalDateTime.now();
+    static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private long employeeId;
     private String name;
     private String department;
@@ -10,8 +17,11 @@ public class Employee {
     private double totalHours;
     private double regularsHours;
     private double overtimeHours;
+    private double punchIn;
+    private double punchOut;
 
-    public Employee(long employeeId, String name, String department, double payRate, double hoursWorked, double totalPay, double totalHours, double regularsHours, double overtimeHours) {
+
+    public Employee(long employeeId, String name, String department, double payRate, double hoursWorked, double totalPay, double totalHours, double regularsHours, double overtimeHours, double punchIn, double punchOut) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
@@ -21,6 +31,29 @@ public class Employee {
         this.totalHours = totalHours;
         this.regularsHours = regularsHours;
         this.overtimeHours = overtimeHours;
+        this.punchIn = punchIn;
+        this.punchOut = punchOut;
+    }
+
+    public static void punchTimeCard() {
+        System.out.println("""
+                Please choose one of the following.
+                1) Check In
+                2) Check Out
+                """);
+        String userChoice = myScanner.nextLine();
+        switch (userChoice) {
+            case "1":
+                System.out.println("Welcome " + timeFormat.format(timeNow));
+                break;
+            case "2":
+                System.out.println("Goodbye " + timeFormat.format(timeNow));
+            default:
+                System.out.println("Choose 1 or 2 ");
+
+        }
+
+
     }
 
     public long getEmployeeId() {
@@ -67,7 +100,7 @@ public class Employee {
         if (overtimeHours < 0) {
             return regularsHours * payRate;
         } else {
-            return (regularsHours * payRate) + (overtimeHours * payRate) * 1.5;
+            return (regularsHours * payRate) + (overtimeHours * payRate * 1.5);
         }
     }
 
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -6,52 +6,59 @@ public class Room {
     private boolean roomOccupied;
     private boolean roomDirty;
     private boolean roomAvailable;
+    private boolean checkedIn;
+    private boolean checkedOut;
+    private boolean cleanRoom;
 
-    public Room(int numberOfBeds, double roomPrice, boolean roomOccupied, boolean roomDirty, boolean roomAvailable) {
+    public Room(int numberOfBeds, double roomPrice, boolean roomOccupied, boolean roomDirty, boolean roomAvailable,boolean checkedIn,boolean checkedOut,boolean cleanRoom) {
         this.numberOfBeds = numberOfBeds;
         this.roomPrice = roomPrice;
         this.roomOccupied = roomOccupied;
         this.roomDirty = roomDirty;
         this.roomAvailable = roomAvailable;
+        this.checkedIn = checkedIn;
+        this.checkedOut = checkedOut;
+        this.cleanRoom = cleanRoom;
     }
 
     public int getNumberOfBeds() {
         return numberOfBeds;
     }
+    public void checkedIn(){
+        roomDirty = true;
+        roomOccupied = true;
+        roomAvailable = false;
+    }
+    public void isCheckedOut(){
+        if(!roomDirty){
+            roomDirty = false;
+            roomOccupied = false;
+        }
+        else{
+            cleanRoom();
+            isCheckedOut();
+        }
+        roomAvailable = true;
+    }
 
-    public void setNumberOfBeds(int numberOfBeds) {
-        this.numberOfBeds = numberOfBeds;
+    public void cleanRoom() {
+        roomDirty = false;
     }
 
     public double getRoomPrice() {
         return roomPrice;
     }
 
-    public void setRoomPrice(double roomPrice) {
-        this.roomPrice = roomPrice;
-    }
-
     public boolean isRoomOccupied() {
         return roomOccupied;
-    }
-
-    public void setRoomOccupied(boolean roomOccupied) {
-        this.roomOccupied = roomOccupied;
     }
 
     public boolean isRoomDirty() {
         return roomDirty;
     }
 
-    public void setRoomDirty(boolean roomDirty) {
-        this.roomDirty = roomDirty;
-    }
-
     public boolean isRoomAvailable() {
         return roomAvailable;
     }
 
-    public void setRoomAvailable(boolean roomAvailable) {
-        this.roomAvailable = roomAvailable;
-    }
 }
