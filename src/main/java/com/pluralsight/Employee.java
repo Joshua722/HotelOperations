2559308665711339cac099f7662520ad42e61116
Joshua <Joshua32149@gmail.com>
2023-10-31 14:26:15 -0400

    Exercise 3

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -7,7 +7,7 @@ import java.time.*;
 public class Employee {
     static Scanner myScanner = new Scanner(System.in);
     static LocalDateTime timeNow = LocalDateTime.now();
-    static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    static DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm:a");
     private long employeeId;
     private String name;
     private String department;
@@ -19,6 +19,7 @@ public class Employee {
     private double overtimeHours;
     private double punchIn;
     private double punchOut;
+    static double decimalTime = timeNow.getHour() + (timeNow.getMinute() / 60);
 
 
     public Employee(long employeeId, String name, String department, double payRate, double hoursWorked, double totalPay, double totalHours, double regularsHours, double overtimeHours, double punchIn, double punchOut) {
@@ -36,26 +37,38 @@ public class Employee {
     }
 
     public static void punchTimeCard() {
-        System.out.println("""
-                Please choose one of the following.
-                1) Check In
-                2) Check Out
-                """);
-        String userChoice = myScanner.nextLine();
-        switch (userChoice) {
-            case "1":
-                System.out.println("Welcome " + timeFormat.format(timeNow));
-                break;
-            case "2":
-                System.out.println("Goodbye " + timeFormat.format(timeNow));
-            default:
-                System.out.println("Choose 1 or 2 ");
+        while (true) {
+            System.out.println("""
+                    Please choose one of the following.
+                    1) Check In
+                    2) Check Out
+                    3) Exit
+                    """);
+            String userChoice = myScanner.nextLine();
+            switch (userChoice) {
+                case "1":
+                    punchIn();
+                    break;
+                case "2":
+                    punchOut();
+                    break;
+                case "3":
+                    System.exit(0);
+                default:
+                    System.out.println("Choose 1 or 2 ");
 
+            }
         }
 
-
     }
-
+    public static void punchIn(double overtimeHours){}
+    public static void punchIn(){
+        System.out.println("Welcome, you've clocked in at " + timeFormat.format(timeNow) + " " + decimalTime);
+    }
+    public static void punchOut(double overtimeHours){}
+    public static void punchOut(){
+        System.out.println("Goodbye, you've clocked out at  " + timeFormat.format(timeNow) + " " + decimalTime);
+    }
     public long getEmployeeId() {
         return employeeId;
     }
